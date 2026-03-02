5322f584f812df1918d78a255981a64d4d8d6fc9
nikiveron <nikiveron@bk.ru>
2026-03-02 17:14:36 +0400

    updated gitignore

--- backend/AudioProcessing.Domain/Entities/FileUpload/FileUploadRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace AudioProcessing.Domain.Entities.FileUpload;
-
-public class FileUploadRequest
-{
-    public string Filename { get; set; } = string.Empty;
-}
--- workers/AudioProcessing.Worker/Program.cs
+++
@@ -1,9 +0,0 @@
-using AudioProcessing.Worker;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-builder.Services.AddHostedService<AudioProcessing.Worker.Services.JobConsumerService>();
-
-var host = builder.Build();
-host.Run();
